ed4e437664c14be43591e389d527e9d756578ad7
REDMOND\dragosav <dragosav@microsoft.com>
2018-04-25 21:09:49 -0700

    initial commit

---
+++ AzureKeyVaultVNetSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureKeyVaultVNetSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
